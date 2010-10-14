0bf5976200e5864ef66f69f24144280c9111efdc
Martin Steel <martin@mediaburst.co.uk>
2010-10-14 10:33:12 +0100

    License tweaks

--- GSMEncoding.cs
+++ GSMEncoding.cs
@@ -6,7 +6,7 @@ using System.Text;
 /*
  * Copyright (c) 2010 Mediaburst Ltd <hello@mediaburst.co.uk>
  *
- * Permission to use, copy, modify, and distribute this software for any
+ * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
